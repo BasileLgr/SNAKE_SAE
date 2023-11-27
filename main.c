3a8d58b4e06c613bd1e91e8801ae66e536807624
Basile Legrelle <basilelegrelle@gmail.com>
2023-11-27 15:09:45 +0100

    :rocket: Deploy Stuff

--- main.c
+++ main.c
@@ -7,11 +7,12 @@
 #define DECALAGE_MENU_DG 80
 #define DECALAGE_MENU_BAS 60
 #define CYCLE 1000000L
+#define CYCLE_MINUTE 60000000L
 
 int main() {
-    char chaine[240];
+    char chaine[180];
     int position = 0;
-    for (int i = 1; i <= 59; ++i) {
+    for (int i = 0; i <= 59; ++i) {
         position += sprintf(chaine + position, "%02d ", i);
     }
     chaine[position - 1] = '\0';
@@ -23,21 +24,22 @@ int main() {
     ChoisirCouleurDessin(CouleurParComposante(25,200,30));
     RemplirRectangle(40,15,NB_COLONNES*TAILLE_CASE,NB_LIGNES*TAILLE_CASE);
     ChoisirCouleurDessin(CouleurParNom("white"));
-    EcrireTexte(40,570,"00:00",2);
+    EcrireTexte(40,570,"00:",2);
     unsigned long suivant = Microsecondes() + CYCLE;
-    char seconde = '1';
-    while (seconde<'9'+1) {
-
+    int i = 0;
+    while (1){
         if (Microsecondes() > suivant) {
             ChoisirCouleurDessin(CouleurParNom("black"));
-            RemplirRectangle(40,540,NB_COLONNES*TAILLE_CASE,NB_LIGNES*TAILLE_CASE+DECALAGE_MENU_BAS);
+            RemplirRectangle(82, 540, NB_COLONNES * TAILLE_CASE, NB_LIGNES * TAILLE_CASE + DECALAGE_MENU_BAS);
             ChoisirCouleurDessin(CouleurParNom("white"));
-            EcrireTexte(95,570,&seconde,2);
-            EcrireTexte(40,570,"00:0",2);
+            EcrireTexte(82, 570, &chaine[i], 2);
+            i = i+3; //décalage dans le tableau
             ChoisirCouleurDessin(CouleurParNom("black"));
-            RemplirRectangle(110,540,NB_COLONNES*TAILLE_CASE,NB_LIGNES*TAILLE_CASE+DECALAGE_MENU_BAS);
+            RemplirRectangle(115, 540, NB_COLONNES * TAILLE_CASE, NB_LIGNES * TAILLE_CASE + DECALAGE_MENU_BAS);
             suivant = Microsecondes() + CYCLE;
-            seconde++;
+        }
+        if (i == 180){
+            i = 0;
         }
     }
     Touche();
