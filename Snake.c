4d060db1f8c723a021de964c9c2e7591da8a6264
Basile Legrelle <basilelegrelle@gmail.com>
2023-12-24 01:15:36 +0100

    :memo:

--- Snake.c
+++ Snake.c
@@ -203,8 +203,8 @@ void fonctionsSnake(int position_seconde, int position_minute) {
             /*Met en pause si on appuie sur espace*/
             if (touche == XK_space) {
                 OnOff = 0;
-                CopierZone (0, 2, 320, 213, 320, 213, 215, 145);
-                CopierZone(1, 0, 320, 213, 320, 213, 215, 145);
+                CopierZone (0, 2, 320, 213, 215, 145, 323, 213);
+                CopierZone(1, 0, 320, 213, 215, 145, 323, 213);
 
                 /*Reprise si on rappuie sur espace*/
                 while (OnOff == 0) {
